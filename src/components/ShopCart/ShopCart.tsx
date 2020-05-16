58c5651006b095c695b2ffd3b6915c3ab7063c25
Federico Peralta <peralta.federico.manuel@gmail.com>
2020-05-16 15:09:30 -0300

    link to /orders/:id

--- src/components/ShopCart/ShopCart.tsx
+++ src/components/ShopCart/ShopCart.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react'
+import { Link } from 'react-router-dom'
 import { Order } from '../Order'
 import { Order as OrderInterface } from '../../interfaces'
 import * as Styled from './ShopCart.styles'
 
 interface ShopCart {
   orders: OrderInterface[]
+  linkTo: string
 }
 
-export const ShopCart: FC<ShopCart> = ({ orders }: ShopCart) => (
+export const ShopCart: FC<ShopCart> = ({ orders, linkTo }: ShopCart) => (
   <>
     {orders.map((order, index) => (
-      <Styled.OrderContainer key={`order${index}`}>
-        <Order items={order.items} totalPrices={order.totalPrices} hideButton={true} />
-      </Styled.OrderContainer>
+      <Link to={`${linkTo}/${index}`} key={`order${index}`}>
+        <Styled.OrderContainer>
+          <Order items={order.items} totalPrices={order.totalPrices} hideButton={true} />
+        </Styled.OrderContainer>
+      </Link>
     ))}
   </>
 )
